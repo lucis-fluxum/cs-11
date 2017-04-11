f4d68b301aca5b45ce9a74dc55f58a8840bc93f6
Luc Street <lucis-fluxum@users.noreply.github.com>
2017-04-10 17:56:09 -0700

    Do Assignment 10.1

--- include/sequence.h
+++ include/sequence.h
@@ -1,8 +1,8 @@
 /**
  * @author Lucas Street
- * @date 4/3/17
+ * @date 4/9/17
  * CS 11, Dave Harden
- * Assignment 9.1 - sequence.h
+ * Assignment 10.1 - sequence.h
  */
 
 #ifndef CS_11_SEQUENCE_H
@@ -16,11 +16,14 @@ namespace cs_sequence {
         typedef int value_type;
         typedef std::size_t size_type;
         sequence();
+        sequence(const sequence &other);
+        ~sequence();
+        sequence &operator=(const sequence &other);
         void start();
         void advance();
         void insert(const value_type &entry);
-//        void attach(const value_type &entry);
-//        void remove_current();
+        void attach(const value_type &entry);
+        void remove_current();
         size_type size() const;
         bool is_item() const;
         value_type current() const;
@@ -34,6 +37,14 @@ namespace cs_sequence {
         node *precursor;
         node *cursor;
         node *tailptr;
+        void copyList(const sequence &other);
+        void copy_cursors(const sequence &other);
+        void prepend_to_sequence(node *new_item);
+        void append_to_sequence(node *new_item);
+        void delete_current_at_head();
+        void delete_current_at_tail();
+        void delete_current_with_backup();
+        void deleteList();
     };
 }
 
--- src/sequence.cpp
+++ src/sequence.cpp
@@ -1,8 +1,8 @@
 /**
  * @author Lucas Street
- * @date 4/3/17
+ * @date 4/9/17
  * CS 11, Dave Harden
- * Assignment 9.1 - sequence.cpp
+ * Assignment 10.1 - sequence.cpp
  */
 
 #include <cassert>
@@ -15,6 +15,47 @@ namespace cs_sequence {
         cursor = NULL;
         precursor = NULL;
         tailptr = NULL;
+    }
+
+
+
+
+
+
+    sequence::sequence(const sequence &other) {
+        headptr = precursor = cursor = tailptr = NULL;
+        num_items = other.num_items;
+        if (num_items > 0) {
+            copyList(other);
+            copy_cursors(other);
+        }
+    }
+
+
+
+
+
+
+    sequence::~sequence() {
+        deleteList();
+    }
+
+
+
+
+
+
+    sequence &sequence::operator=(const sequence &other) {
+        if (this != &other) {
+            deleteList();
+            sequence *tmp = new sequence(other);
+            num_items = tmp->num_items;
+            headptr = tmp->headptr;
+            precursor = tmp->precursor;
+            cursor = tmp->cursor;
+            tailptr = tmp->tailptr;
+        }
+        return *this;
     }
 
 
@@ -48,22 +89,58 @@ namespace cs_sequence {
 
     void sequence::insert(const value_type &entry) {
         num_items++;
-        node *tempptr = new node;
-        tempptr->data = entry;
+        node *new_item = new node;
+        *new_item = {entry, NULL};
 
         if (headptr == NULL) {
-            tempptr->next = NULL;
-            headptr = tempptr;
-            tailptr = tempptr;
-        } else if (cursor == NULL || cursor == headptr) {
-            tempptr->next = headptr;
-            headptr = tempptr;
+            headptr = new_item;
+            tailptr = new_item;
+        } else if (!is_item() || cursor == headptr) {
+            prepend_to_sequence(new_item);
         } else {
-            tempptr->next = cursor;
-            precursor->next = tempptr;
+            new_item->next = cursor;
+            precursor->next = new_item;
         }
+        cursor = new_item;
+    }
 
-        cursor = tempptr;
+
+
+
+
+
+    void sequence::attach(const value_type &entry) {
+        if (headptr == NULL) {
+            insert(entry);
+        } else if (!is_item() || cursor == tailptr) {
+            num_items++;
+            node *new_item = new node;
+            *new_item = {entry, NULL};
+            append_to_sequence(new_item);
+        } else {
+            advance();
+            insert(entry);
+        }
+    }
+
+
+
+
+
+
+    void sequence::remove_current() {
+        assert(is_item());
+        num_items--;
+        if (headptr == tailptr) {
+            delete cursor;
+            cursor = precursor = headptr = tailptr = NULL;
+        } else if (cursor == headptr) {
+            delete_current_at_head();
+        } else if (cursor == tailptr) {
+            delete_current_at_tail();
+        } else {
+            delete_current_with_backup();
+        }
     }
 
 
@@ -92,5 +169,129 @@ namespace cs_sequence {
     sequence::value_type sequence::current() const {
         assert(is_item());
         return cursor->data;
+    }
+
+
+
+
+    // Private interface -----------------------------------------------------------------
+
+    void sequence::copyList(const sequence &other) {
+        headptr = new node;
+        tailptr = headptr;
+        const node *iterator = other.headptr;
+        while (iterator != other.tailptr) {
+            tailptr->data = iterator->data;
+            iterator = iterator->next;
+            tailptr->next = new node;
+            tailptr = tailptr->next;
+        }
+        tailptr->data = other.tailptr->data;
+        tailptr->next = NULL;
+    }
+
+
+
+
+
+
+    void sequence::copy_cursors(const sequence &other) {
+        const node *iterator = other.headptr;
+        node *temp = headptr;
+        while (iterator != NULL) {
+            if (iterator == other.precursor) {
+                precursor = temp;
+                cursor = temp->next;
+                break;
+            } else if (iterator == other.cursor) {
+                cursor = temp;
+                break;
+            }
+            iterator = iterator->next;
+            temp = temp->next;
+        }
+    }
+
+
+
+
+
+
+    void sequence::prepend_to_sequence(node *new_item) {
+        new_item->next = headptr;
+        headptr = new_item;
+    }
+
+
+
+
+
+
+    void sequence::append_to_sequence(node *new_item) {
+        precursor = tailptr;
+        tailptr->next = new_item;
+        tailptr = new_item;
+        cursor = new_item;
+    }
+
+
+
+
+
+
+    void sequence::delete_current_at_head() {
+        cursor = cursor->next;
+        delete headptr;
+        headptr = cursor;
+    }
+
+
+
+
+
+
+    void sequence::delete_current_at_tail() {
+        delete cursor;
+        tailptr = precursor;
+        tailptr->next = NULL;
+        cursor = precursor = NULL;
+    }
+
+
+
+
+
+
+    /**
+     * Explanation: assuming current is somewhere in the middle of 3+ items:
+     *
+     * [o,p] -> [c] -> []  // make a backup of the precursor
+     *
+     * [o,p]  [c] -> []    // link the backup to the node after current
+     *   |___________|
+     *
+     * [o] [p] -> [c]      // advance one node forward
+     *  |__________|
+     *
+     * [o,p] -> [c]        // delete precursor node and set it to the backup
+     */
+    void sequence::delete_current_with_backup() {
+        node *backup = precursor;
+        backup->next = cursor->next;
+        advance();
+        delete precursor;
+        precursor = backup;
+    }
+
+
+
+
+
+
+    void sequence::deleteList() {
+        start();
+        while (is_item()) {
+            remove_current();
+        }
     }
 }
